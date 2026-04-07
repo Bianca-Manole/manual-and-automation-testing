10048f03b239986790519153403713c24c8af26d
Bianca <bianca_manole@outlook.com>
2026-04-07 21:05:09 +0300

    Added tests for element states, alerts, modals, keyboard actions and attributes

--- automation/junit-exercises/src/test/java/DemoTool.java
+++ automation/junit-exercises/src/test/java/DemoTool.java
@@ -1,13 +1,11 @@
 import io.github.bonigarcia.wdm.WebDriverManager;
-import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.Keys;
 
 import java.time.Duration;
 
@@ -43,7 +41,7 @@ public class DemoTool {
         driver.quit(); // closes all tabs
     }
 
-    @Test
+    @Test @Disabled // @Disabled will ignore the tests
     public void registerUser() throws InterruptedException {
         driver.get("https://demoqa.com/text-box"); //driver.get(String url)
         Thread.sleep(3000); //pauses the execution for x ms - adds exception
@@ -70,6 +68,123 @@ public class DemoTool {
 
         Thread.sleep(5000);
         //WebElement emailParagraph = driver.findElement(By.id("email"));
+    }
+
+    @Test
+    public void alerts() throws InterruptedException {
+        driver.get("https://demoqa.com/alerts");
+
+        //simple alert
+        WebElement alertButton = driver.findElement(By.id("alertButton"));
+        alertButton.click();
+        Thread.sleep(1000);
+
+        Assertions.assertEquals("You clicked a button", driver.switchTo().alert().getText()); // because we can find an alert through HTML, we use switchTo().alert() to make the switch and get the text
+
+        Thread.sleep(1000);
+        driver.switchTo().alert().accept(); // to click the "OK" or "Accept" button from the alert
+        //you can click user .dismiss() instead of .accept() to select "Cancel" or "No"
+        Thread.sleep(1000);
+        //prompt alert
+        WebElement promptButton = driver.findElement(By.id("promtButton"));
+        promptButton.click();
+        Thread.sleep(1000);
+
+        Assertions.assertEquals("Please enter your name", driver.switchTo().alert().getText());
+
+        Thread.sleep(3000);
+
+        driver.switchTo().alert().sendKeys("bianca"); // to input text in the alert field
+
+        Thread.sleep(3000);
+        driver.switchTo().alert().accept();
+
+        Thread.sleep(1000);
+    }
+
+    @Test @Disabled
+    public void modals() {
+        //verify that "Small Modal" modal is opened with text"This is a small modal. It has very less content"
+        //and that the close button is enabled
+
+        driver.get("https://demoqa.com/modal-dialogs");
+        WebElement smallModalButton = driver.findElement(By.id("showSmallModal"));
+        smallModalButton.click();
+
+        WebElement modalTitle = driver.findElement(By.id("example-modal-sizes-title-sm"));
+        WebElement modalContent = driver.findElement(By.xpath("//div[@class=\"modal-body\"]"));
+        WebElement closeButton = driver.findElement(By.id("closeSmallModal"));
+
+        Assertions.assertEquals("Small Modal", modalTitle.getText()); //check if the expected title "Small Modal" is the same with the actual modalTtitle.getText() - which gets the title of the element
+        Assertions.assertEquals("This is a small modal. It has very less content", modalContent.getText()); //check if the expected context "This is a small modal. It has very less content"
+        //is the same with the actual modalContent.getText() - which gets the content of the element
+        Assertions.assertTrue(closeButton.isEnabled()); // checks if the button is enabled - checks the state of the element
+    }
+
+
+    @Test @Disabled
+    public void keyboardActions() throws InterruptedException {
+        driver.get("https://demoqa.com/text-box");
+        Thread.sleep(3000);
+
+
+        WebElement currentAddressTextarea = driver.findElement(By.id("currentAddress"));
+        WebElement permanentAddressTextarea = driver.findElement(By.id("permanentAddress"));
+
+        currentAddressTextarea.sendKeys("vine vacanta"); // input the text
+        currentAddressTextarea.sendKeys(Keys.CONTROL+"A"); // ctrl+a - selects the text
+        currentAddressTextarea.sendKeys(Keys.CONTROL+"C"); // ctrl+c - copies the text
+        currentAddressTextarea.sendKeys(Keys.TAB); // tab- move to next area
+        permanentAddressTextarea.sendKeys(Keys.CONTROL+"V"); // ctrl+v - pastes the text
+        Thread.sleep(3000);
+
+        //currentAddressTextarea.clear(); // empties the field
+
+        currentAddressTextarea.sendKeys(Keys.CONTROL+"A");
+        currentAddressTextarea.sendKeys(Keys.DELETE);
+        Thread.sleep(3000);
+
+        for(int i = 0; i < 10; i++) {
+            permanentAddressTextarea.sendKeys(Keys.BACK_SPACE);
+        }
+        Thread.sleep(3000);
+    }
+
+
+    @Test @Disabled // element states
+    public void elementState() throws InterruptedException {
+        driver.get("https://demoqa.com/text-box");
+        Thread.sleep(3000);
+
+        WebElement header = driver.findElement(By.xpath("//h1"));
+        //is displayed
+        System.out.println(header.isDisplayed());
+        //is enabled
+        System.out.println(header.isEnabled());
+        //is selected
+        System.out.println(header.isSelected());
+    }
+
+
+
+    @Test @Disabled
+    public void elementAttributes()throws InterruptedException {
+        driver.get("https://demoqa.com/text-box");
+        Thread.sleep(3000);
+
+        WebElement header = driver.findElement(By.xpath("//h1"));
+
+        //get text
+        System.out.println(header.getText());
+        //get tag
+        System.out.println(header.getTagName());
+        //get location
+        System.out.println(header.getLocation());
+        //get size
+        System.out.println(header.getSize());
+        //get attribute
+        System.out.println(header.getDomAttribute("class"));
+
     }
 
 }
